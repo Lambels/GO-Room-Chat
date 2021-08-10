b330265d7d63387e1b290f89777571974c20b7a1
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-10 14:18:44 +0300

    support for the indexWS

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,14 +3,30 @@ package handlers
 import (
 	"GO-Group-Chat/internal/config"
 	"GO-Group-Chat/internal/helpers"
+	"GO-Group-Chat/internal/middleware"
+	"GO-Group-Chat/internal/models"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var (
 	app *config.AppConfig
+
+	upgrader = websocket.Upgrader {
+		WriteBufferSize: 1024,
+		ReadBufferSize: 1024,
+	}
+
+	regChan 	chan<- models.WsConnection
+	unRegChan 	chan<- models.WsConnection
+	broadChan 	chan<- *models.Message
 )
+
+func init() {
+	regChan, unRegChan, broadChan = models.NewConnStore()
+}
 
 func InitHandlers(a *config.AppConfig) {
 	app = a
@@ -18,6 +34,8 @@ func InitHandlers(a *config.AppConfig) {
 
 // Handles "/" and renders to different templates depending if the user
 // sending the request is auth
+//
+// and accepts only the GET verb
 func Index(w http.ResponseWriter, r *http.Request) {
 	if !helpers.IsAuth(r) {
 		err := helpers.RenderTemplate(w, "index.html", nil)
@@ -36,6 +54,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Couldn't render template", http.StatusInternalServerError)
 	}
+}
+
+// Handels "/index/ws" and connects to the index room used to feed users on the index page updates such as
+// new rooms
+//
+// IndexWS needs to be protected by onlyAuth middleware
+func IndexWS(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	// browser doesent support ws ?
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
+
+	user := r.Context().Value(middleware.AccountKey{})
+
+	conn := models.WsConnection {
+		Conn: 	ws,
+		Acc: 	user.(models.Account),
+		Send: 	make(chan *models.Message),
+	}
+
+	go conn.Listen(regChan, unRegChan, broadChan)
 }
 
 // Signup handles "/signup" and accepts POST and GET verbs
@@ -99,6 +140,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	helpers.RenderTemplate(w, "login.html", nil)
 }
 
+// Logout handles "/logout" and accepts the GET verb
+//
+// Logout needs to be protected by onlyAUth middleware
 func Logout(w http.ResponseWriter, r *http.Request) {
 	err := helpers.Logout(w, r)
 	if err != nil {
