2303538c81f79aa35c707f6761fe7c05c6796f59
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-12 14:45:20 +0300

    *Account -> Account

--- internal/models/message.go
+++ internal/models/message.go
@@ -12,7 +12,7 @@ type Message struct {
 	Type 		int								`json:"type"`
 
 	// Sender is the account which send the message
-	Sender		*Account						`json:"account"`
+	Sender		Account							`json:"account"`
 
 	// The data is map and its processed by the reciever in different ways
 	// depending on what type the message is
