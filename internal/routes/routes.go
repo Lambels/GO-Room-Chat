fe165f44017fe2edba4ea9634111864cd9610864
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-14 13:03:55 +0300

    new room + join room routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -23,9 +23,11 @@ func InitHandlers() http.Handler {
 	authProtected.HandleFunc("/index/ws", handlers.IndexWS)
 
 	authProtected.HandleFunc("/logout", handlers.Logout).Methods(http.MethodGet)
-	// TODO: Implement rooms and handlers
+	
 	authProtected.HandleFunc("/rooms/new", handlers.CreateRoom).Methods(http.MethodGet, http.MethodPost)
-	//authProtected.HandleFunc("/rooms/{pk:[0-9]+}", handlers.JoinRoom).Methods(http.MethodGet)
+	authProtected.HandleFunc("/rooms/{pk:[0-9]+}", handlers.JoinRoom).Methods(http.MethodGet)
+	authProtected.HandleFunc("/rooms/{pk:[0-9]+}/ws", handlers.JoinRoomWS)
+	// TODO: Implement rooms and handlers
 	authProtected.HandleFunc("/rooms/delete/{pk}", nil).Methods(http.MethodGet, http.MethodDelete)
 
 	unAuthProtected.HandleFunc("/signup", handlers.Signup).Methods(http.MethodGet, http.MethodPost)
