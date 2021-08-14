e5c036000b5f6c290433b97f0d5ce5a5f98d108d
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-14 13:23:15 +0300

    more configarable mysql connection

--- cmd/web/setup-app.go
+++ cmd/web/setup-app.go
@@ -15,14 +15,24 @@ import (
 
 func SetupApp() (*config.AppConfig) {
 	dbName := flag.String("dbName", "testdb", "Database name")
+	dbUser := flag.String("dbUser", "root", "Databse user")
+	dbPass := flag.String("dbPass", "", "Database password")
+	dbPort := flag.String("dbPort", "3306", "Database port")
+	dbHost := flag.String("dbHost", "localhost", "Database host")
 	stName := flag.String("stName", "sessions", "Sessions table name")
 	port := flag.String("port", ":8080", "Port on wich the server listens on")
 	
 	flag.Parse()
 
-	dsnString := fmt.Sprintf("root:@tcp(localhost:3306)/%s?parseTime=true&loc=Local", *dbName)
-
 	log.Println("setupApp: Initializing database")
+	var dsnString string
+
+	if *dbPass == "" {
+		dsnString = fmt.Sprintf("%s:@tcp(%s:%s)/%s?parseTime=true&loc=Local", *dbUser, *dbHost, *dbPort, *dbName)
+	} else {
+		dsnString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=Local", *dbUser, *dbPass, *dbHost, *dbPort, *dbName)
+	}
+
 	db, err := driver.ConnectMySQL(dsnString)
 	if err != nil {
 		log.Fatal(err)
