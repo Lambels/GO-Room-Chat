7eab5ee5c1b5bd5a65d5247cc14a44b9debd54c8
Patrick Arvatu <patrick.arvatu@yahoo.com>
2021-08-14 13:00:35 +0300

    support active connections

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,15 +2,17 @@ package config
 
 import (
 	"GO-Group-Chat/internal/driver"
+	"GO-Group-Chat/internal/models"
 	"html/template"
 
 	"github.com/srinathgs/mysqlstore"
 )	
 
 type AppConfig struct {
-	Templates 		*template.Template
-	Store			*mysqlstore.MySQLStore
-	DB 				*driver.DB
-	Domain 			string
-	Version			string
+	Templates 			*template.Template
+	Store				*mysqlstore.MySQLStore
+	DB 					*driver.DB
+	Domain 				string
+	Version				string
+	ActiveConnections	map[int64]models.CommunicationChannels
 }
